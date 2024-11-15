c2bae3da4c043b296899d2bddbe296139cad4a61
msiwy12345 <mateuszsiwy00@gmail.com>
2024-11-15 22:20:55 +0100

    updated getCities method

--- WikiGuesser.Server/Controllers/WikipediaController.cs
+++ WikiGuesser.Server/Controllers/WikipediaController.cs
@@ -1,4 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 
 namespace WikiGuesser.Server.Controllers
 {
@@ -13,12 +15,11 @@ namespace WikiGuesser.Server.Controllers
             _httpClient = httpClient;
         }
 
-        // Get: api/Wikipedia/GetRandomArticle
         [HttpGet("article/{title}")]
         public async Task<IActionResult> GetArticle(string title)
         {
             // Budowanie URL zapytania do API Wikipedii
-            var url = $"https://en.wikipedia.org/w/api.php?action=query&prop=revisions&rvslots=main&format=json&titles={title}&rvprop=content";
+            var url = $"https://en.wikipedia.org/w/api.php?action=parse&format=json&page={title}&prop=text";
 
             try
             {
@@ -45,59 +46,67 @@ namespace WikiGuesser.Server.Controllers
         [HttpGet("cities")]
         public async Task<IActionResult> GetCities()
         {
-            // Budowanie URL zapytania do API Wikipedii
-            var url = "https://en.wikipedia.org/w/api.php?action=query&titles=List_of_countries&prop=extracts&exintro=true&format=json";
-
-            try
+            List<string> countryCodes = new List<string>
+                {
+                    "AF", "AL", "DZ", "AD", "AO", "AG", "AR", "AM", "AU", "AT", "AZ",
+                    "BS", "BH", "BD", "BB", "BY", "BE", "BZ", "BJ", "BT", "BO", "BA",
+                    "BW", "BR", "BN", "BG", "BF", "BI", "CV", "KH", "CM", "CA", "CF",
+                    "TD", "CL", "CN", "CO", "KM", "CD", "CR", "HR", "CU", "CY", "CZ",
+                    "DK", "DJ", "DM", "DO", "EC", "EG", "SV", "GQ", "ER", "EE", "SZ",
+                    "ET", "FJ", "FI", "FR", "GA", "GM", "GE", "DE", "GH", "GR", "GD",
+                    "GT", "GN", "GW", "GY", "HT", "HN", "HU", "IS", "IN", "ID", "IR",
+                    "IQ", "IE", "IL", "IT", "JM", "JP", "JO", "KZ", "KE", "KI", "KP",
+                    "KR", "KW", "KG", "LA", "LV", "LB", "LS", "LR", "LY", "LI", "LT",
+                    "LU", "MG", "MW", "MY", "MV", "ML", "MT", "MH", "MR", "MU", "MX",
+                    "FM", "MD", "MC", "MN", "ME", "MA", "MZ", "MM", "NA", "NR", "NP",
+                    "NL", "NZ", "NI", "NE", "NG", "MK", "NO", "OM", "PK", "PW", "PS",
+                    "PA", "PG", "PY", "PE", "PH", "PL", "PT", "QA", "RO", "RU", "RW",
+                    "KN", "LC", "VC", "WS", "SM", "ST", "SA", "SN", "RS", "SC", "SL",
+                    "SG", "SK", "SI", "SB", "SO", "ZA", "SS", "ES", "LK", "SD", "SR",
+                    "SE", "CH", "SY", "TJ", "TZ", "TH", "TL", "TG", "TO", "TT", "TN",
+                    "TR", "TM", "TV", "UG", "UA", "AE", "GB", "US", "UY", "UZ", "VU",
+                    "VE", "VN", "YE", "ZM", "ZW"
+                };
+            List<string> cities = new List<string>();
+            foreach(var code in countryCodes)
             {
-                // Wysyłanie zapytania GET do API Wikipedii
-                var response = await _httpClient.GetAsync(url);
-
-                if (response.IsSuccessStatusCode)
+                cities = await GetRandomCitiesFromCountry(code);
+                if (cities.Count > 0)
                 {
-                    // Odczytanie odpowiedzi
-                    var content = await response.Content.ReadAsStringAsync();
-                    return Ok(content);
-                }
-                else
-                {
-                    return NotFound("Artykuł nie został znaleziony.");
+                    return Ok(cities);
                 }
             }
-            catch
-            {
-                return StatusCode(500, "Błąd podczas pobierania artykułu z Wikipedii.");
-            }
+            return Ok(cities);
         }
 
-        [HttpGet("articleCity")]
-        public async Task<IActionResult> GetCityArticle()
+        public async Task<List<string>> GetRandomCitiesFromCountry(string country)
         {
-            // Budowanie URL zapytania do API Wikipedii
-            var url = "https://en.wikipedia.org/w/api.php?action=query&titles=Gwangmyeong&prop=extracts&explaintext=true&format=json";
-
+            var url = $"http://api.geonames.org/searchJSON?country={country}&featureClass=P&maxRows=100&username=msiwy";
 
             try
             {
-                // Wysyłanie zapytania GET do API Wikipedii
                 var response = await _httpClient.GetAsync(url);
 
                 if (response.IsSuccessStatusCode)
                 {
-                    // Odczytanie odpowiedzi
                     var content = await response.Content.ReadAsStringAsync();
-                    return Ok(content);
+                    JObject json = JObject.Parse(content); 
+                    var cities = json["geonames"].Select(x => x["name"].ToString()).ToList();
+
+                    Random random = new Random();
+                    return cities.OrderBy(x => random.Next()).Take(10).ToList();
                 }
                 else
                 {
-                    return NotFound("Artykuł nie został znaleziony.");
+                    return new List<string>();
                 }
             }
             catch
             {
-                return StatusCode(500, "Błąd podczas pobierania artykułu z Wikipedii.");
+                return new List<string>();
             }
         }
+
 
     }
 }
